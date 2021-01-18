8fdc6c6d57f1b2fd1c6cdef575a1057fe19174bc
Horea <maxeh@protonmail.com>
2021-01-18 20:42:30 +0100

    genres in progress

--- webApp/src/app/pages/genres/genres.component.ts
+++ webApp/src/app/pages/genres/genres.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TaskService } from 'src/app/task.service';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-genres',
@@ -8,19 +9,28 @@ import { TaskService } from 'src/app/task.service';
 })
 export class GenresComponent implements OnInit {
   request = "genres/";
-
-  constructor(private TaskService: TaskService) { }
+  arr: string[] = []
+  constructor(private TaskService: TaskService, private http: HttpClient) { 
+  }
 
   ngOnInit(): void {
   }
 
   getName(name : string) {
+    this.arr = []
     if(name != "") {
       /*check if the parameters names (actor_name, director_name) are like 
       in the endpoints doc */
       this.request += "?actor=" + name + "&director=" + name;
       let jsonMess = this.TaskService.dataRequest(this.request);
     }
+    this.TaskService.dataRequest(this.request).toPromise().then(data => {
+      console.log(data);
+
+      for (let key in data) 
+         if (data.hasOwnProperty(key))
+           this.arr.push(data[key]);
+    });
     this.request = "genres/";
   }
 }
