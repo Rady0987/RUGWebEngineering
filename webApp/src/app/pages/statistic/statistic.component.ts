cdf4523ec587f2e20cd76029972e818b38d05736
Cainarean Constatnin <cainarean.c1@gmail.com>
2021-01-19 22:11:03 +0100

    added statistcs page

--- webApp/src/app/pages/statistic/statistic.component.ts
+++ webApp/src/app/pages/statistic/statistic.component.ts
@@ -1,25 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { TaskService } from 'src/app/task.service';
 
+interface Statistics {
+  mean_popularity : number;
+  median_popularity : number;
+  standard_deviation_popularity : number;
+}
+
 @Component({
   selector: 'app-statistic',
   templateUrl: './statistic.component.html',
   styleUrls: ['./statistic.component.css']
 })
+
 export class StatisticComponent implements OnInit {
   request = "actorstatistics/"
-
-  constructor(private TaskService: TaskService) { }
+  items : Statistics;
+  constructor(private TaskService: TaskService) {
+    this.items = {} as Statistics;
+  }
 
   ngOnInit(): void {
   }
 
   getName(name : string) {
+    this.items = {} as Statistics;
     if(name != "") {
         /*check if the parameters names (actor_name, director_name) are like 
         in the endpoints doc */
-      this.request += "?actor_name=" + name + "&director_name=" + name;
-      let jsonMess = this.TaskService.dataRequest(this.request);
+      this.request += name;
+      this.TaskService.dataRequest(this.request).subscribe(data  => this.items = <Statistics> data);
     }
     this.request = "actorstatistics/";
   }
