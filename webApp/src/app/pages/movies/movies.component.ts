b921826c4d0e91025cea656d86ecf5f4b7e10ff6
Denis Garabajiu <denis1610@yahoo.com>
2021-01-19 23:14:52 +0200

    Ampersand Bug Fix

--- webApp/src/app/pages/movies/movies.component.ts
+++ webApp/src/app/pages/movies/movies.component.ts
@@ -25,18 +25,18 @@ export class MoviesComponent implements OnInit {
     if(actor != "" || title != "" || director != "" || year != "") {
       this.request += `${(title != "") ? ("&title=" + title) : ""}`;
       this.request = this.andOperator(this.request, actor);
-      this.request += `${(actor != "") ? ("&actor=" + actor) : ""}`;
+      this.request += `${(actor != "") ? ("actor=" + actor) : ""}`;
       this.request = this.andOperator(this.request, director);
-      this.request += `${(director != "") ? ("&director=" + director) : ""}`;
+      this.request += `${(director != "") ? ("director=" + director) : ""}`;
       this.request = this.andOperator(this.request, year);
-      this.request += `${(year != "") ? ("&year=" + year) : ""}`;
+      this.request += `${(year != "") ? ("year=" + year) : ""}`;
     }
     console.log(this.request);
     this.TaskService.movieDataRequest(this.request).subscribe(data => this.items = data);
   }
 
   andOperator(request : string, param : any) {
-    if(request != "movies/?details&limit=50" && (param != "" || param != 0))
+    if(param != "" || param != 0)
       request += "&";
     return request;
   }
